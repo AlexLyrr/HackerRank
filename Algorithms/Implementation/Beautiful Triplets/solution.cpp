7a5ca02e41e81af38785a5cf8405a321f464fa24
Alex Lyr <alex_gfd@hotmail.com>
2018-12-16 17:36:40 +0100

    Create solution.cpp

---
+++ Algorithms/Implementation/Beautiful Triplets/solution.cpp
@@ -0,0 +1,96 @@
+/*
+    Problem: https://www.hackerrank.com/challenges/beautiful-triplets/problem 
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the beautifulTriplets function below.
+int beautifulTriplets(int d, vector<int> arr) {
+    
+    std::set<int> s(arr.begin(), arr.end());
+    int sumOfTriplets = 0;
+
+    for (auto &element : s){
+
+        auto it1 = s.find(element + d);
+        auto it2 = s.find(element + 2*d);
+
+        if (it1 != s.end() && it2 != s.end()){
+          sumOfTriplets += count(arr.begin(), arr.end(), element) *
+                           count(arr.begin(), arr.end(), element + d) *
+                           count(arr.begin(), arr.end(), element + 2 * d);
+          cout << "Triplet found with elements: " << element << ", "
+               << element + d << "," << element + 2 * d << endl;
+        }        
+    }
+
+    return sumOfTriplets;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = beautifulTriplets(d, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
